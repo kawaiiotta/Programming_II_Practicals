d9e9d4c7fadf2320d129ee1f03e222e4424b904d
kawaiiotta <80732113+kawaiiotta@users.noreply.github.com>
2021-08-02 20:26:48 +0800

    Refactor of password checking code

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,11 +1,24 @@
 """
 This code will Error check a password
 """
-password = str(input("Set new password: "))
-while not len(password) > 6:
-    print("Password is not long enough")
+
+
+def main():
+    password = get_password()
+    print_asterix(password)
+
+
+def get_password():
     password = str(input("Set new password: "))
+    while not len(password) > 6:
+        print("Password is not long enough")
+        password = str(input("Set new password: "))
+    return password
 
-for char in password:
-    print("*", end="")
 
+def print_asterix(password):
+    for char in password:
+        print("*", end="")
+
+
+main()
